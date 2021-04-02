88071fcfaed7bf06218b475eaf588fec88e39743
Aswanth S <61882638+AshCoder232@users.noreply.github.com>
2021-04-02 18:04:23 +0530

    Update spambot1.py

--- spambot1.py
+++ spambot1.py
@@ -3,11 +3,12 @@ import time
 
 def Sendmsg():
   time.sleep(10);
-  text=open("mrrobot.txt")
-  for line1 in text:
+  filename=input('enter file name:')
+  fh=open("filename.txt")
+  for line1 in fh:
     pyautogui.typewrite(line1)
     pyautogui.press('enter')
-    time.sleep(0.8)
+    time.sleep(0.1)
 
 Sendmsg()
 
